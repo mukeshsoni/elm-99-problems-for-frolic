8037abd1be67d247da84deef78822dc6705e3f0a
Mukesh Soni <mukeshsoni@gmail.com>
2016-08-14 23:18:59 +0530

    added one more problem

---
+++ problem_22.elm
@@ -0,0 +1,6 @@
+-- Create a list containing all integers within a given range, inclusively, allow for reverse order
+
+range : Int -> Int -> List Int 
+range start end = 
+    -- your implementation goes here
+    []
